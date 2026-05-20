9dceac54ef25958d5574caf12a71054e01a3a980
M <muhammed@Muhammeds-MacBook-Pro.local>
2026-05-20 23:33:57 +0530

    Backend ready

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 import express from "express";
 
 import cors from "cors";
+import dotenv from "dotenv";
 
+dotenv.config();
 import twilio from "twilio";
 import dotenv from "dotenv";
 
@@ -58,4 +60,9 @@ app.listen(5000, () => {
   console.log(
     "WhatsApp server running on port 5000"
   );
+});
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on ${PORT}`);
 });
